73e6cceacad2d7bc08558cd931d527027967b98c
Puskar10 <puskarshaw346@gmail.com>
2026-05-06 22:18:18 +0530

    go

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const URL =
+  import.meta.env.MODE === "production"
+    ? "https://bluemeet.onrender.com"
+    : "http://localhost:5000";
+
+const socket = io(URL, {
   autoConnect: true,
 });
 
